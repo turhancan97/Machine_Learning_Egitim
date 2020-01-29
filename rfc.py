ee0c63b58bed523ce7bd447fe92a2413657ce1db
Turhan Can Kargın <turhancan.kargin@gmail.com>
2020-01-29 09:40:53 +0300

    Add files via upload

---
+++ rfc.py
@@ -0,0 +1,38 @@
+#1. kutuphaneler
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#2. Veri Onisleme
+
+#2.1. Veri Yukleme
+veriler = pd.read_csv('veriler.csv')
+#pd.read_csv("veriler.csv")
+
+x = veriler.iloc[:,1:4].values #bağımsız değişkenler
+y = veriler.iloc[:,4:].values #bağımlı değişken
+print(y)
+
+#verilerin egitim ve test icin bolunmesi
+from sklearn.model_selection import train_test_split
+x_train, x_test,y_train,y_test = train_test_split(x,y,test_size=0.33, random_state=0)
+
+#verilerin olceklenmesi
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train = sc.fit_transform(x_train)
+X_test = sc.transform(x_test)
+
+from sklearn.ensemble import RandomForestClassifier
+rfc = RandomForestClassifier(n_estimators=15, criterion = 'gini') #entropi de kullanılabilir.
+rfc.fit(X_train,y_train)
+
+y_pred = rfc.predict(X_test)
+cm = confusion_matrix(y_test,y_pred)
+print('RFC')
+print(cm)
+
+
+    
+
---
+++ ödev3.py
@@ -0,0 +1,4 @@
+#2. Veri Onisleme
+
+#2.1. Veri Yukleme
+veriler = pd.read_excel('iris.xls')
